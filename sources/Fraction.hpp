aaf1a8aedc4c0b9e6f041aed6972cfa649638287
RenanaTurgeman <renanaturgeman058@gmail.com>
2023-04-16 13:15:42 +0300

    create fraction.hpp

---
+++ sources/Fraction.hpp
@@ -0,0 +1,38 @@
+#ifndef FRACTION_HPP
+#define FRACTION_HPP
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+namespace ariel{
+class Fraction {
+    private:
+        int numerator;
+        int denominator;
+    public:
+        // Constructors
+        Fraction(int num = 0, int den = 1) : numerator(num), denominator(den) {}
+        Fraction(float f);
+        // Overloaded operators
+    Fraction operator+(const Fraction& other) const;
+    Fraction operator-(const Fraction& other) const;
+    Fraction operator*(const Fraction& other) const;
+    Fraction operator/(const Fraction& other) const;
+    bool operator==(const Fraction& other) const;
+    bool operator>(const Fraction& other) const;
+    bool operator<(const Fraction& other) const;
+    bool operator>=(const Fraction& other) const;
+    bool operator<=(const Fraction& other) const;
+    Fraction& operator++(); // prefix increment
+    const Fraction operator++(int); // postfix increment
+    Fraction& operator--(); // prefix decrement
+    const Fraction operator--(int); // postfix decrement
+    ostream& operator<<(ostream& os, const Fraction& f);
+    istream& operator>>(istream& is, Fraction& f);
+
+
+};
+
+};
+#endif
